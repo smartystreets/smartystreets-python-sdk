feafe1fcf4bd8402632113764378adfae1ac90a9
Neo Pimentel <neo@smartystreets.com>
2016-09-14 15:23:51 -0600

    We now prep requests after build_request() instead of in it.

--- smartystreets_python_sdk/requests_sender.py
+++ smartystreets_python_sdk/requests_sender.py
@@ -8,22 +8,22 @@ class RequestsSender:
         self.session = Session()
         self.max_timeout = max_timeout
 
-    def send(self, smartyrequest):
-        request = self.build_request(smartyrequest)
-
-        response = self.session.send(request, timeout=self.max_timeout)
-
-        return self.build_smartyresponse(response)
-
-    def build_request(self, smartyrequest):
-        request = Request(url=smartyrequest.urlprefix, params=smartyrequest.parameters)
-        request.headers['User-Agent'] = "smartystreets (sdk:python@" + Version.CURRENT + ")"
-        if smartyrequest.referer is not None:
-            request.headers['Referer'] = smartyrequest.referer
-        request.data = smartyrequest.payload
-        request.method = "GET" if smartyrequest.payload is None else "POST"
+    def send(self, smarty_request):
+        request = self.build_request(smarty_request)
         prepped_request = self.session.prepare_request(request)
-        return prepped_request
 
-    def build_smartyresponse(self, inner_response):
+        response = self.session.send(prepped_request, timeout=self.max_timeout)
+
+        return self.build_smarty_response(response)
+
+    def build_request(self, smarty_request):
+        request = Request(url=smarty_request.urlprefix, params=smarty_request.parameters)
+        request.headers['User-Agent'] = "smartystreets (sdk:python@" + Version.CURRENT + ")"
+        if smarty_request.referer is not None:
+            request.headers['Referer'] = smarty_request.referer
+        request.data = smarty_request.payload
+        request.method = "GET" if smarty_request.payload is None else "POST"
+        return request
+
+    def build_smarty_response(self, inner_response):
         return Response(inner_response.content, inner_response.status_code)
