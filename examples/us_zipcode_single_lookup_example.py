84019d7d7ac5545416af1318055f60d26c24ae03
Neo Pimentel <neo@smartystreets.com>
2016-09-20 09:50:24 -0600

    Wrote us_zipcode_single_lookup_example.py

---
+++ examples/us_zipcode_single_lookup_example.py
@@ -0,0 +1,36 @@
+import os
+
+from smartystreets_python_sdk import StaticCredentials
+from smartystreets_python_sdk.us_zipcode import ClientBuilder, Lookup
+
+
+def run():
+    auth_id = os.environ['SMARTY_AUTH_ID']  # We recommend storing your keys in environment variables
+    auth_token = os.environ['SMARTY_AUTH_TOKEN']
+    credentials = StaticCredentials(auth_id, auth_token)
+
+    client = ClientBuilder(credentials).build()
+
+    lookup = Lookup()
+    lookup.city = "Mountain View"
+    lookup.state = "California"
+
+    client.send_lookup(lookup)
+
+    result = lookup.result
+    zipcodes = result.zipcodes
+    cities = result.cities
+
+    for city in cities:
+        print "\nCity: " + city.city
+        print "State: " + city.state
+        print "Mailable City: {}".format(city.mailable_city)
+
+    for zipcode in zipcodes:
+        print "\nZIP Code: " + zipcode.zipcode
+        print "Latitude: {}".format(zipcode.latitude)
+        print "Longitude: {}".format(zipcode.longitude)
+
+
+if __name__ == "__main__":
+    run()
