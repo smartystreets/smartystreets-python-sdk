d135e176f6aded01388b9e0bd69d0ffd5d124f49
Neo Pimentel <neo@smartystreets.com>
2016-09-20 15:00:49 -0600

    Lookup now accepts all fields as optional parameters

--- smartystreets_python_sdk/us_street/lookup.py
+++ smartystreets_python_sdk/us_street/lookup.py
@@ -1,15 +1,16 @@
 class Lookup:
-    def __init__(self, freeform=None):
+    def __init__(self, street=None, street2=None, secondary=None, city=None, state=None, zipcode=None, lastline=None,
+                 addressee=None, urbanization=None, match=None, candidates=1, input_id=None):
         self.result = []
-        self.input_id = None
-        self.street = freeform
-        self.street2 = None
-        self.secondary = None
-        self.city = None
-        self.state = None
-        self.zipcode = None
-        self.lastline = None
-        self.addressee = None
-        self.urbanization = None
-        self.match = None
-        self.candidates = 1
+        self.input_id = input_id
+        self.street = street
+        self.street2 = street2
+        self.secondary = secondary
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.lastline = lastline
+        self.addressee = addressee
+        self.urbanization = urbanization
+        self.match = match
+        self.candidates = candidates
--- smartystreets_python_sdk/us_zipcode/lookup.py
+++ smartystreets_python_sdk/us_zipcode/lookup.py
@@ -1,7 +1,7 @@
 class Lookup:
-    def __init__(self, city=None, state=None, zipcode=None):
+    def __init__(self, city=None, state=None, zipcode=None, input_id=None):
         self.result = None
-        self.input_id = None
+        self.input_id = input_id
         self.city = city
         self.state = state
         self.zipcode = zipcode
