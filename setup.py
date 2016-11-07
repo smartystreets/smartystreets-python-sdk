02021a50f7889e6b9f66a0488af270e78b3641ce
Neo Pimentel <neo@smartystreets.com>
2016-11-07 09:53:30 -0700

    Added Setup file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='smartystreets_python_sdk',
+    packages=['smartystreets_python_sdk'],
+    version='0.0.0',
+    description='A library to help Python developers easily access the SmartyStreets APIs',
+    author='SmartyStreets SDK Team',
+    url='<Whatever the Github repo ends up being>',
+    download_url='<The URL to download the source code from Github>',
+    keywords=['smartystreets', 'smarty', 'address', 'validation', 'verification', 'street', 'sdk', 'library']
+)
