3e76ca4fd9b7f16cc195de015371e41b531354f3
Neo Pimentel <neo@smartystreets.com>
2016-11-09 16:16:43 -0700

    Added author email to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.0.0',
     description='A library to help Python developers easily access the SmartyStreets APIs',
     author='SmartyStreets SDK Team',
+    author_email='support@smartystreets.com',
     url='https://github.com/smartystreets/smartystreets-python-sdk',
     download_url='<The URL to download the source code from Github>',
     keywords=['smartystreets', 'smarty', 'address', 'validation', 'verification', 'street', 'sdk', 'library']
