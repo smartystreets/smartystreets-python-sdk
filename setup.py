851294bdb16b50552760bc175b29fff2dd86c49a
Neo Pimentel <neo@smartystreets.com>
2016-11-15 12:31:33 -0700

    Added information to setup.py.

--- setup.py
+++ setup.py
@@ -6,9 +6,19 @@ setup(
     packages=['smartystreets_python_sdk'],
     version=smarty.__version__,
     description='A library to help Python developers easily access the SmartyStreets APIs',
+    long_description='Official Python library for SmartyStreets',
     author='SmartyStreets SDK Team',
     author_email='support@smartystreets.com',
+    license='Apache 2',
     url='https://github.com/smartystreets/smartystreets-python-sdk',
     download_url='https://github.com/smartystreets/smartystreets-python-sdk/tarball/'+smarty.__version__,
-    keywords=['smartystreets', 'smarty', 'address', 'validation', 'verification', 'street', 'sdk', 'library']
+    keywords=['smartystreets', 'smarty', 'address', 'validation', 'verification', 'street', 'sdk', 'library'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License, version 2.0',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ]
 )
