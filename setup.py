30d4cbd7f20e8e1f2b92b249fa823682e751fa9c
Neo Pimentel <neo@smartystreets.com>
2016-11-16 11:26:31 -0700

    Fixed a classifier in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License, version 2.0',
+        'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3'
