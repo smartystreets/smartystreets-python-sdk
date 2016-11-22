486da703dc94085c693e70de3392cff0900d4bd6
Neo Pimentel <neo@smartystreets.com>
2016-11-22 15:46:45 -0700

    Wording change.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='smartystreets_python_sdk',
     packages=['smartystreets_python_sdk'],
     version='0.0.0',
-    description='A library to help Python developers easily access the SmartyStreets APIs',
+    description='An official library to help Python developers easily access the SmartyStreets APIs',
     long_description='Official Python library for SmartyStreets',
     author='SmartyStreets SDK Team',
     author_email='support@smartystreets.com',
