954f0ad1167538a8a42f69b385aaebdd90b7aac8
Neo Pimentel <neo@smartystreets.com>
2017-03-06 11:02:49 -0700

    Implemented extract’s Address class.

--- smartystreets_python_sdk/us_extract/__init__.py
+++ smartystreets_python_sdk/us_extract/__init__.py
@@ -1,3 +1,4 @@
 from .lookup import Lookup
 from .result import Result
 from .metadata import Metadata
+from .address import Address
---
+++ smartystreets_python_sdk/us_extract/address.py
@@ -0,0 +1,15 @@
+from smartystreets_python_sdk.us_street.candidate import Candidate
+
+
+class Address:
+    def __init__(self, obj):
+        self.text = obj.get('text', None)
+        self.verified = obj.get('verified', None)
+        self.line = obj.get('line', None)
+        self.start = obj.get('start', None)
+        self.end = obj.get('end', None)
+        candidates = obj.get('api_output', [])
+        self.api_output = []
+
+        for candidate in candidates:
+            self.api_output.append(Candidate(candidate))
