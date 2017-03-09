799170cf5b5ef6d808e047b1cd7ecd75fbcf1c29
Neo Pimentel <neo@smartystreets.com>
2017-03-09 16:03:11 -0700

    Added international Lookup class.

---
+++ smartystreets_python_sdk/international_street/lookup.py
@@ -0,0 +1,17 @@
+class Lookup:
+    def __init__(self):
+        self.result = []
+
+        self.inputId = None
+        self.country = None
+        self.geocode = None
+        self.language = None
+        self.freeform = None
+        self.address1 = None
+        self.address2 = None
+        self.address3 = None
+        self.address4 = None
+        self.organization = None
+        self.locality = None
+        self.administrativeArea = None
+        self.postalCode = None
