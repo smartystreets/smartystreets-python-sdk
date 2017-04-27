ef98f1d6d8b910e9757adf56b541406a25834e29
Neo Pimentel <neo@smartystreets.com>
2017-04-27 09:18:40 -0600

    Minor whitespace adjustment.

--- smartystreets_python_sdk/us_zipcode/lookup.py
+++ smartystreets_python_sdk/us_zipcode/lookup.py
@@ -3,6 +3,7 @@ class Lookup:
         """
         In addition to holding all of the input data for this lookup, this class also
             will contain the result of the lookup after it comes back from the API.
+            
             See "https://smartystreets.com/docs/cloud/us-zipcode-api#http-request-input-fields"
         """
         self.result = None
