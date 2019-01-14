fae138cddd88b4a40439613f67ba62e0fe87fb0a
Michael Whatcott <mdwhatcott@gmail.com>
2019-01-14 15:08:51 -0700

    Reformatted file.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import smartystreets_python_sdk_version
+from smartystreets_python_sdk_version import __version__
 
 import os
 from distutils.core import setup
@@ -8,17 +8,33 @@ if hasattr(os, 'link'):
 
 setup(
     name='smartystreets_python_sdk',
-    packages=['smartystreets_python_sdk', 'smartystreets_python_sdk.us_street', 'smartystreets_python_sdk.us_zipcode',
-              'smartystreets_python_sdk.us_extract', 'smartystreets_python_sdk.us_autocomplete', 'smartystreets_python_sdk.international_street'],
-    version=smartystreets_python_sdk_version.__version__,
+    packages=[
+        'smartystreets_python_sdk',
+        'smartystreets_python_sdk.us_street',
+        'smartystreets_python_sdk.us_zipcode',
+        'smartystreets_python_sdk.us_extract',
+        'smartystreets_python_sdk.us_autocomplete',
+        'smartystreets_python_sdk.international_street',
+    ],
+    version=__version__,
     description='An official library to help Python developers easily access the SmartyStreets APIs',
     long_description='Official Python library for SmartyStreets',
     author='SmartyStreets SDK Team',
     author_email='support@smartystreets.com',
     license='Apache 2',
     url='https://github.com/smartystreets/smartystreets-python-sdk',
-    download_url='https://github.com/smartystreets/smartystreets-python-sdk/tarball/' + smartystreets_python_sdk_version.__version__,
-    keywords=['smartystreets', 'smarty', 'address', 'validation', 'verification', 'street', 'sdk', 'library', 'geocode'],
+    download_url='https://github.com/smartystreets/smartystreets-python-sdk/tarball/' + __version__,
+    keywords=[
+        'smartystreets',
+        'smarty',
+        'address',
+        'validation',
+        'verification',
+        'street',
+        'sdk',
+        'library',
+        'geocode',
+    ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
