1f25b3bd8ebfd18e66ef0cc97bc932143626ea4d
Michael Whatcott <mdwhatcott@gmail.com>
2021-03-24 16:37:09 -0600

    Using key instead of auth-id for embedded (website) key authentication.

--- smartystreets_python_sdk/shared_credentials.py
+++ smartystreets_python_sdk/shared_credentials.py
@@ -4,5 +4,5 @@ class SharedCredentials:
         self.host_name = host_name
 
     def sign(self, request):
-        request.parameters['auth-id'] = self.id
+        request.parameters['key'] = self.id
         request.referer = "https://" + self.host_name
