05cf3ddce73b476f3a472f77a584ec90322fd0a4
Caroline Hickey <caroline@smartystreets.com>
2022-08-01 16:11:40 -0600

    Reverted hard coded versioning

--- smartystreets_python_sdk_version/__init__.py
+++ smartystreets_python_sdk_version/__init__.py
@@ -1,1 +1,1 @@
-__version__ = '4.11.3'  # DO NOT EDIT (this is updated by a build job when a new release is published)
+__version__ = '0.0.0'  # DO NOT EDIT (this is updated by a build job when a new release is published)
