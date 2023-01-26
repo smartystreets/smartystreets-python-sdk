2a852ebddf7ccaf110b9d8df9a6fccaa5b539bab
Ryan Cox <ryan@smarty.com>
2023-01-25 17:20:21 -0700

    Use the mock sender so we don't actually try to send a request and throw exceptions in the test.

--- test/license_test.py
+++ test/license_test.py
@@ -1,11 +1,12 @@
 import unittest
 import smartystreets_python_sdk as smarty
+from test.mocks import *
 
 
 class TestLicenseSender(unittest.TestCase):
 
     def test_license_set(self):
-        sender = smarty.RequestsSender()
+        sender = MockSender(None)
         licenses = ['one', 'two', 'three']
         sender = smarty.LicenseSender(licenses, sender)
         request = smarty.Request()
@@ -16,7 +17,7 @@ class TestLicenseSender(unittest.TestCase):
         self.assertEqual('one,two,three', request.parameters['license'])
 
     def test_license_not_set(self):
-        sender = smarty.RequestsSender()
+        sender = MockSender(None)
         sender = smarty.LicenseSender([], sender)
         request = smarty.Request()
         request.url_prefix = "http://localhost"
