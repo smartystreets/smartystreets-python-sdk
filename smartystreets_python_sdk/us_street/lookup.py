d6c18a5c5a89e77b60d3949d7fad36ff9edf54b0
Andrew Johnson <andrew.johnson@smarty.com>
2023-10-19 14:45:12 -0600

    Changed the order of the format output

--- smartystreets_python_sdk/us_street/lookup.py
+++ smartystreets_python_sdk/us_street/lookup.py
@@ -4,8 +4,8 @@ from smartystreets_python_sdk.us_street.output_format import OutputFormat
 
 class Lookup:
     def __init__(self, street=None, street2=None, secondary=None, city=None, state=None, zipcode=None, lastline=None,
-                 addressee=None, urbanization=None, match=MatchType.STRICT, outputformat=OutputFormat.DEFAULT,
-                 candidates=1, input_id=None):
+                 addressee=None, urbanization=None, match=MatchType.STRICT, candidates=1, input_id=None,
+                 outputformat=OutputFormat.DEFAULT):
         """
         In addition to holding all input data for this lookup, this class also will contain the result
         of the lookup after it comes back from the API.
