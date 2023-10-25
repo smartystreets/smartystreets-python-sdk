2bd3fef8ba00caeeda8d5582cd74da89f270d100
Landon Fackrell <landon@smarty.com>
2023-10-25 12:11:51 -0600

    Add tests

---
+++ test/us_enrichment/client_test.py
@@ -0,0 +1,42 @@
+import unittest
+
+from smartystreets_python_sdk import URLPrefixSender
+from smartystreets_python_sdk.us_enrichment.client import Client, send_lookup
+from smartystreets_python_sdk.us_enrichment.lookup import FinancialLookup, PrincipalLookup
+from smartystreets_python_sdk.us_enrichment.response import Response
+from test.mocks import *
+
+
+class TestClient(unittest.TestCase):
+    def test_sending_Financial_Lookup(self):
+        capturing_sender = RequestCapturingSender()
+        sender = URLPrefixSender('http://localhost/', capturing_sender)
+        serializer = FakeSerializer(None)
+        client = Client(sender, serializer)
+        lookup = FinancialLookup("xxx")
+        result = send_lookup(client, lookup)
+
+        self.assertEqual("property", lookup.dataset)
+        self.assertEqual("financial", lookup.dataSubset)
+        self.assertEqual(Response, type(result))
+        self.assertEqual(lookup.result, result)
+
+        function_result = client.send_property_financial_lookup("xxx")
+        self.assertEqual(result, function_result)
+
+    def test_sending_principal_lookup(self):
+        capturing_sender = RequestCapturingSender()
+        sender = URLPrefixSender('http://localhost/', capturing_sender)
+        serializer = FakeSerializer(None)
+        client = Client(sender, serializer)
+
+        lookup = PrincipalLookup("xxx")
+        result = send_lookup(client, lookup)
+
+        self.assertEqual("property", lookup.dataset)
+        self.assertEqual("principal", lookup.dataSubset)
+        self.assertEqual(Response, type(result))
+        self.assertEqual(lookup.result, result)
+
+        function_result = client.send_property_principal_lookup("xxx")
+        self.assertEqual(result, function_result)
