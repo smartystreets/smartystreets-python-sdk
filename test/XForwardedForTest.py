e85267718641c727d57b66e0182e3b76196804b0
Spencer  Jorgensen <spencer@smarty.com>
2024-05-28 10:10:01 -0600

    Switched the test url to include just localhost.

--- test/XForwardedForTest.py
+++ test/XForwardedForTest.py
@@ -6,11 +6,10 @@ from test.mocks import *
 class XForwardedForTest (unittest.TestCase):
     def testNativeSetOnQuery(self):
         smartyrequest = smarty.Request()
-        smartyrequest.url_prefix = "http://192.168.1.243:3128"
+        smartyrequest.url_prefix = "http://localhost"
         smartyrequest.payload = "Test Payload"
         sender = smarty.RequestsSender(10000,None,"0.0.0.0")
         request = smarty.RequestsSender.build_request(sender,smartyrequest)
-        response = sender.send(request)
         
         self.assertEqual("0.0.0.0",request.headers['X-Forwarded-For'])
         
@@ -18,10 +17,11 @@ class XForwardedForTest (unittest.TestCase):
 
     def testNativeNotSet(self):
         smartyrequest = smarty.Request()
-        smartyrequest.url_prefix = "http://192.168.1.243:3128"
+        smartyrequest.url_prefix = "http://localhost"
         smartyrequest.payload = "Test Payload"
         sender = smarty.RequestsSender()
         request = smarty.RequestsSender.build_request(sender,smartyrequest)
-        sender.send(request)
-        self.assertFalse('X-Forwarded-For' in request.headers)
         
+        self.assertFalse('X-Forwarded-For' in request.headers)
+
+
