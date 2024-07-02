74a70ca155e60b5d9081ef2e8e9b49635d73b4ae
Bryan Amundson <bryan@smartystreets.com>
2024-07-02 15:39:06 -0600

    Added new administrative area fields in international autocomplete.

--- smartystreets_python_sdk/international_autocomplete/candidate.py
+++ smartystreets_python_sdk/international_autocomplete/candidate.py
@@ -3,6 +3,8 @@ class Candidate:
         self.street = obj.get('street', None)
         self.locality = obj.get('locality', None)
         self.administrative_area = obj.get('administrative_area', None)
+        self.administrative_area_short = obj.get('administrative_area_short', None)
+        self.administrative_area_long = obj.get('administrative_area_long', None)
         self.postal_code = obj.get('postal_code', None)
         self.country_iso3 = obj.get('country_iso3', None)
         # v2 fields
