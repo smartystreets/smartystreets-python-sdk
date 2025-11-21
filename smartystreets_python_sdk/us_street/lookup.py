a50a6624f99335012892be76ada0840de8168081
Eric Devenport <eric@smarty.com>
2025-11-21 15:31:40 -0700

    Allow users to specify candidates=1 with match=enhanced

--- smartystreets_python_sdk/us_street/lookup.py
+++ smartystreets_python_sdk/us_street/lookup.py
@@ -4,7 +4,7 @@ from smartystreets_python_sdk.us_street.output_format import OutputFormat
 
 class Lookup:
     def __init__(self, street=None, street2=None, secondary=None, city=None, state=None, zipcode=None, lastline=None,
-                 addressee=None, urbanization=None, match=MatchType.STRICT, candidates=1, input_id=None,
+                 addressee=None, urbanization=None, match=MatchType.STRICT, candidates=0, input_id=None,
                  outputformat=OutputFormat.DEFAULT, county_source=None):
         """
         In addition to holding all input data for this lookup, this class also will contain the result
