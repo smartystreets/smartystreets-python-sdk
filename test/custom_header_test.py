22aacbd6245552283fc28354f1283fc6fe87e254
Ryan Cox <ryan@smarty.com>
2026-05-04 09:49:52 -0600

    Add regression test for custom_header erasing upstream sender state.

--- test/custom_header_test.py
+++ test/custom_header_test.py
@@ -49,6 +49,20 @@ class TestCustomHeaderSender(unittest.TestCase):
         self.assertEqual('Test-Agent', builder.header['User-Agent'])
         self.assertEqual('Test-Type', builder.header['Content-Type'])
 
+    def test_send_forwards_smarty_request_with_upstream_state_intact(self):
+        inner = MockSender(None)
+        sender = smarty.CustomHeaderSender({'X-Test': 'value'}, inner)
+        smartyrequest = smarty.Request()
+        smartyrequest.url_prefix = "http://localhost"
+        smarty.SharedCredentials('shared-id', 'example.com').sign(smartyrequest)
+
+        sender.send(smartyrequest)
+
+        self.assertIs(smartyrequest, inner.request)
+        self.assertEqual('https://example.com', inner.request.referer)
+        self.assertEqual('shared-id', inner.request.parameters['key'])
+        self.assertEqual('value', inner.request.headers['X-Test'])
+
     def test_appended_headers_are_joined_with_separator(self):
         sender = smarty.CustomHeaderSender(
             {'User-Agent': ['base-value', 'custom-value']},
