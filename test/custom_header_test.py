7e1a42182d4543034fe1d49edebd6f173c2240f3
Ryan Cox <ryan@smarty.com>
2026-05-04 09:56:43 -0600

    Collapse redundant custom_header tests and exercise send in test_populates_headers.

--- test/custom_header_test.py
+++ test/custom_header_test.py
@@ -6,40 +6,17 @@ from test.mocks import *
 class TestCustomHeaderSender(unittest.TestCase):
 
     def test_populates_headers(self):
-        sender = smarty.RequestsSender()
-        header = {'Test-User-Agent': 'Test-Agent', 'Test-Content-Type': 'Test-Content-Type'}
-        sender = smarty.CustomHeaderSender(header, sender)
-
-        self.assertEqual(sender.headers['Test-User-Agent'], 'Test-Agent')
-        self.assertEqual(sender.headers['Test-Content-Type'], 'Test-Content-Type')
-
-    def test_custom_headers_set(self):
         sender = smarty.CustomHeaderSender(
             {'Test-User-Agent': 'Test-Agent', 'Test-Content-Type': 'Test-Type'},
             MockSender(None)
         )
         smartyrequest = smarty.Request()
         smartyrequest.url_prefix = "http://localhost"
-        smartyrequest.payload = "This is the test content."
 
         sender.send(smartyrequest)
 
         self.assertEqual('Test-Agent', smartyrequest.headers['Test-User-Agent'])
         self.assertEqual('Test-Type', smartyrequest.headers['Test-Content-Type'])
-
-    def test_custom_headers_used(self):
-        sender = smarty.CustomHeaderSender(
-            {'User-Agent': 'Test-Agent', 'Content-Type': 'Test-Type'},
-            MockSender(None)
-        )
-        smartyrequest = smarty.Request()
-        smartyrequest.url_prefix = "http://localhost"
-        smartyrequest.payload = "This is the test content."
-
-        sender.send(smartyrequest)
-
-        self.assertEqual('Test-Agent', smartyrequest.headers['User-Agent'])
-        self.assertEqual('Test-Type', smartyrequest.headers['Content-Type'])
 
     def test_multiple_with_custom_header_calls_merge_headers(self):
         builder = smarty.ClientBuilder(None)
